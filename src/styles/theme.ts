dea05a9a31ad447c42d215ce0fd20787add92bac
brouwilliamog <william.pereira@focomultimidia.com>
2022-09-09 19:58:43 -0300

    Iniciando projeto

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,8 +1,10 @@
 const theme = {
   colors: {
-    background: '#121214',
     text: '#e1e1e5',
-    primary: '#8257e6'
+    primary: '#8257e6',
+    ascent: '#dc3545',
+    whiteColor: '#F0F0F0',
+    darkColor: '#1F1F1F',
   }
 };
 
