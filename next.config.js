4a6b4e52c44b8d583ff9fe3ee1bab3415e276650
brouwilliamog <william.pereira@focomultimidia.com>
2022-09-10 00:45:19 -0300

    Para otimização

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 const withImages = require('next-images');
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
-module.exports = withBundleAnalyzer({})
-
 module.exports = withImages({
   esModule: true
 });
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -27,23 +27,6 @@ export function Header() {
         <a className='go-down' ></a>
       </header>
       <div className="slider" ></div>
-      {/* <section>
-      <nav>
-        <h3>co.Blvck</h3>
-      </nav>
-      <section>
-      <section className='home'>
-            <div className='hero'>
-                <img src="https://wnka.co/wp-content/uploads/2018/01/travis-scott-astroworld-2019-cr-Julian-Bajsel-billboard-1548-compressed.jpg" alt=""/>
-                <h1 className='headline'>co.Blvck</h1>
-            </div>
-        </section>
-      </section>
-      <a href="#about_us" className='go-down'></a>
-      </section>
-      <div className="slider">
-
-      </div> */}
     </Container>
   );
 }
