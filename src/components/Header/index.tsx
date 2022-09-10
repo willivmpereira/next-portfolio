f505652605953e83381b993810f2ea501f3ea80d
brouwilliamog <william.pereira@focomultimidia.com>
2022-09-10 00:42:50 -0300

    Para otimização

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,50 +1,21 @@
 import { Container } from './styles';
 import React, {useRef, useEffect} from  'react';
 import Image from 'next/image';
-import { Power2, TimelineLite } from "gsap";
+
 
 
 export function Header() {
 
-  let hero: any = useRef(null);
-  let slide: any = useRef(null);
-  let logo: any = useRef(null);
-  // let menu: any = useRef(null);
-  let godown: any = useRef(null);
-
-  let lt = new TimelineLite();
-  
-
-  useEffect(() => {
-    lt.fromTo(hero,1, {height: "0%"}, {height: "80%", ease: Power2.easeInOut})
-      .fromTo(hero, 1.2, {width: "100%"}, {width: "80%", ease: Power2.easeInOut}, "-=0.3")
-      .fromTo(slide, 1.2, {x: "-100%"}, {x: "0%", ease: Power2.easeInOut}, "-=1.2")
-      .fromTo(logo, 0.5, {opacity: 0, x: 30}, {opacity: 1, x: 0}, "-=0.5")
-      // .fromTo(menu, 0.5, {opacity: 0, x: 30}, {opacity: 1, x: 0}, "-=0.5")
-      .fromTo(godown, 0.5, {opacity: 0, y: 30}, {opacity: 1, y: 0}, "-=0.9")
-    //lt.from(hero, 1, {css: {height: '0%'}});
-    //lt.to(hero, 1, {css: {height: '80%'}});
-
-    // lt.from(hero, 1.2, {css: {width: '100%'}});
-    // lt.to(hero, 1.2, {css: {width: '80%'}});
-
-
-    // lt.from(slide, 1.2, {css: {x: '-100%'}});
-    // lt.to(slide, 1.2, {css: {x: '0%'}});
-
-    // lt.from(logo, 0.5, {css: {opacity: '0', x: 30}});
-    // lt.to(logo, 0.5, {css: {opacity: '1', x: 0}});
-  })
 
   return (
     <Container>
       <header>
         <nav>
-          <h3 ref={el => logo = el}>co.Blvck</h3>
+          <h3>co.Blvck</h3>
          
         </nav>
         <section>
-          <div className="hero" ref={el => hero = el}>
+          <div className="hero">
             <Image
               src="https://wnka.co/wp-content/uploads/2018/01/travis-scott-astroworld-2019-cr-Julian-Bajsel-billboard-1548-compressed.jpg"
               alt="Banner"
@@ -53,9 +24,9 @@ export function Header() {
             <h1 className='headline' >co.Blvck</h1>
           </div>
         </section>
-        <a className='go-down' ref={el => godown = el}></a>
+        <a className='go-down' ></a>
       </header>
-      <div className="slider" ref={el => slide = el}></div>
+      <div className="slider" ></div>
       {/* <section>
       <nav>
         <h3>co.Blvck</h3>
