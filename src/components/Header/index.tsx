e26f43987f56e6b6c0dfa4290b5d3a4a355df00f
brouwilliamog <william.pereira@focomultimidia.com>
2022-09-12 20:52:22 -0300

    Adicionando Animação

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,32 +1,46 @@
-import { Container } from './styles';
-import React, {useRef, useEffect} from  'react';
+import React, { useRef, useEffect } from 'react';
 import Image from 'next/image';
-
-
+import { Container } from './styles';
+import { Power2, TimelineLite } from "gsap";
 
 export function Header() {
 
+  let hero: any = useRef(null);
+  let slide: any = useRef(null);
+  let logo: any = useRef(null);
+  let menu: any = useRef(null);
+  let godown: any = useRef(null);
+
+  let lt = new TimelineLite();
+
+  useEffect(() => {
+    lt.fromTo(hero,1, {height: "0%"}, {height: "80%", ease: Power2.easeInOut})
+      .fromTo(hero, 1.2, {width: "100%"}, {width: "80%", ease: Power2.easeInOut}, "-=0.3")
+      .fromTo(slide, 1.2, {x: "-100%"}, {x: "0%", ease: Power2.easeInOut}, "-=1.2")
+      .fromTo(logo, 0.5, {opacity: 0, x: 30}, {opacity: 1, x: 0}, "-=0.5")
+      //.fromTo(menu, 0.5, {opacity: 0, x: 30}, {opacity: 1, x: 0}, "-=0.5")
+      .fromTo(godown, 0.5, {opacity: 0, y: 30}, {opacity: 1, y: 0}, "-=0.9")
+  })
 
   return (
     <Container>
       <header>
         <nav>
-          <h3>co.Blvck</h3>
-         
+          <h3 ref={el => logo = el}>William</h3>
         </nav>
         <section>
-          <div className="hero">
+          <div className="hero" ref={el => hero = el}>
             <Image
               src="https://wnka.co/wp-content/uploads/2018/01/travis-scott-astroworld-2019-cr-Julian-Bajsel-billboard-1548-compressed.jpg"
               alt="Banner"
               layout="fill"
             />
-            <h1 className='headline' >co.Blvck</h1>
+            <h1 className="headline">William</h1>
           </div>
         </section>
-        <a className='go-down' ></a>
+        <a className="go-down" ref={el => godown = el}></a>
       </header>
-      <div className="slider" ></div>
+      <div className="slider" ref={el => slide = el}></div>
     </Container>
   );
 }
