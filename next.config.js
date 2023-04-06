a5f2a65d61016772a126aaa2b81ad631368c7164
brouwilliamog <william.pereira@focomultimidia.com>
2023-04-05 22:01:55 -0300

    Config imagens local e external

--- next.config.js
+++ next.config.js
@@ -1,16 +1,11 @@
-const withImages = require('next-images');
-
-module.exports = withImages({
-  esModule: true
-});
+const withImages = require('next-images')
 
 module.exports = {
+  ...withImages(),
   images: {
     domains: ['wnka.co', 'i.imgur.com', 'media.graphassets.com', 'upload.wikimedia.org', 'observatoriodocinema.uol.com.br'],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    formats: ['image/png','image/jpeg', 'image/webp'],
   },
   build : { 
     transpile : [ 'gsap' ] 
-  } 
+  }  
 }
